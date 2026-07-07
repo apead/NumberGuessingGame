e44d6d7570ce2d6b6ad37d8f57d1c2b31c227573
adpead <adpead@gmail.com>
2026-07-07 14:19:47 +0200

    Created and Implemented Number Guess Logic and GameResult enum return type

---
+++ NumberGuessingGame/NumberGuessing.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberGuessingGame
+{
+    internal class NumberGuessing
+    {
+        private int numberToGuess;
+        private int minimum;
+        private int maximum;
+
+        public NumberGuessing(int minimum, int maximum)
+        {
+            GenerateRandomNumber(minimum,maximum);
+        }
+
+        public void GenerateRandomNumber(int minimum, int maximum)
+        {
+            Random rand = new Random();
+            numberToGuess = rand.Next(minimum, maximum);
+
+            this.minimum = minimum;
+            this.maximum = maximum;
+        }
+
+        public GuessResult MakeGuess(int guess)
+        {
+            if (guess > maximum)
+            {
+                return GuessResult.OutOfLimits;
+            }
+            else if (guess < minimum)
+            {
+                return GuessResult.OutOfLimits;
+
+            }
+            else
+                if (guess == numberToGuess)
+                {
+                    return GuessResult.Correct;
+                }
+                else if (guess < numberToGuess)
+                {
+                    return GuessResult.TooLow;
+                }
+                else
+                {
+                    return GuessResult.TooHigh;
+                }
+        }
+    }
+}
